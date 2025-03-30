ff8ec1f509eae47340a0944d62b38811213efaee
Santiago Ferraz <santiago.ferraz@hotmail.com>
2025-03-30 03:53:31 -0300

    commit initial

---
+++ src/main/java/com/nava/cooperfilmes_back/CooperfilmesBackApplication.java
@@ -0,0 +1,13 @@
+package com.nava.cooperfilmes_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CooperfilmesBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CooperfilmesBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nava/cooperfilmes_back/CooperfilmesBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nava.cooperfilmes_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CooperfilmesBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
