d03628e1a8115be9535010f4a7333f567533d567
Santiago Ferraz <santiago.ferraz@hotmail.com>
2025-03-31 06:44:08 -0300

    ajustes diversos

--- src/main/java/com/nava/cooperfilmes_back/domain/user/Role.java
+++ src/main/java/com/nava/cooperfilmes_back/domain/user/Role.java
@@ -1,5 +1,6 @@
 package com.nava.cooperfilmes_back.domain.user;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
 CREATE TABLE IF NOT EXISTS tb_roles(role_id BIGINT PRIMARY KEY, name VARCHAR(255));
-INSERT IGNORE INTO tb_roles (role_id, name) VALUES (1, 'analista');
-INSERT IGNORE INTO tb_roles (role_id, name) VALUES (2, 'revisor');
-INSERT IGNORE INTO tb_roles (role_id, name) VALUES (3, 'aprovador');
+INSERT IGNORE INTO tb_roles (role_id, name) VALUES (1, 'ANALISTA');
+INSERT IGNORE INTO tb_roles (role_id, name) VALUES (2, 'REVISOR');
+INSERT IGNORE INTO tb_roles (role_id, name) VALUES (3, 'APROVADOR');
