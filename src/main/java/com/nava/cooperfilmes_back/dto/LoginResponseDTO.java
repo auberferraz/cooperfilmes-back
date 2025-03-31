157b3e0abe52856ea642fde3ffe62cc386b308bd
Santiago Ferraz <santiago.ferraz@hotmail.com>
2025-03-31 20:39:17 -0300

    endpoint de consultar roteiro

--- src/main/java/com/nava/cooperfilmes_back/dto/LoginResponseDTO.java
+++ src/main/java/com/nava/cooperfilmes_back/dto/LoginResponseDTO.java
@@ -1,8 +1,4 @@
 package com.nava.cooperfilmes_back.dto;
 
-import com.nava.cooperfilmes_back.domain.user.Role;
-
-import java.util.Set;
-
 public record LoginResponseDTO(String email, String name, String token, String role) {
 }
